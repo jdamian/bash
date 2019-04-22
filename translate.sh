b4efd7e9f0ae88b38e0bb38d22c60c14497cde60
Juan Damian <jcarlosdamian@gmail.com>
2019-04-22 13:14:32 -0500

    update  translate

--- translate.sh
+++ translate.sh
@@ -9,3 +9,4 @@ text=`xsel -o|trans :es -brief`
 gxmessage -font "sans 9" -geometry 400x600 -wrap $text
 #xsel -o |trans :es -brief |tee|zenity --text-info --width=500 --height=500 --timeout=60
 
+#install package: xsel, translate-shell, gxmessage
