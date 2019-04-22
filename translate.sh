d9d4a8543cfe2905cbd560ed01b7026860e01258
Juan Damian <jcarlosdamian@gmail.com>
2019-04-22 13:07:54 -0500

    translate

---
+++ translate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#author: jdamian
+#last date: 20/11/18
+
+#text=`xsel -o`
+text=`xsel -o|trans :es -brief`
+#text=`xsel -o|trans :es -brief -e bing`
+#echo $text
+gxmessage -font "sans 9" -geometry 400x600 -wrap $text
+#xsel -o |trans :es -brief |tee|zenity --text-info --width=500 --height=500 --timeout=60
+
